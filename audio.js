4856e60164038ae9cf8dfa8189572731e3f48200
mariannecortes123-lgtm <mariannecortes123@gmail.com>
2025-11-18 00:39:34 -0500

    Create audio.js

---
+++ audio.js
@@ -0,0 +1,10 @@
+function speak(text) {
+    if ('speechSynthesis' in window) {
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.rate = 1;
+        utterance.pitch = 1;
+        speechSynthesis.speak(utterance);
+    } else {
+        alert('Your browser does not support text-to-speech.');
+    }
+}
